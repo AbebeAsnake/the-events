51267d400f266a9fceeda5e19eac709fce0ae84e
abebetiku <47992643+abebetiku@users.noreply.github.com>
2019-03-01 17:34:00 -0500

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Container} from 'semantic-ui-react';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import EventDashboard from '../../features/Event/EventDashboard/EventDashboard';
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavBar/>
+      <Container className="main">
+        <EventDashboard/>  
+      </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
