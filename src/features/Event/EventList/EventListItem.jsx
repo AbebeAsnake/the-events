8c4c584452bb4ef836f6c617f4a78c9e538eafae
Abebe Asnake <abebeasnake@gmail.com>
2019-03-03 20:58:48 -0500

    CRUD implemeted

--- src/features/Event/EventList/EventListItem.jsx
+++ src/features/Event/EventList/EventListItem.jsx
@@ -3,7 +3,7 @@ import { Segment, Icon, Button, List, Item } from "semantic-ui-react";
 import EventListAtendee from "./EventListAtendee";
 class EventListItem extends Component {
   render() {
-    const { event } = this.props;
+    const { event, onEventOpen, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -35,7 +35,20 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <Button
+            onClick={onEventOpen(event)}
+            as="a"
+            color="teal"
+            floated="right"
+            content="View"
+          />
+          <Button
+            onClick={deleteEvent(event.id)}
+            as="a"
+            color="red"
+            floated="right"
+            content="Delete"
+          />
         </Segment>
       </Segment.Group>
     );
