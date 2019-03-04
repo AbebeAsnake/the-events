05ca7420b3c0bee67ef5668c9d2d748826edf0f2
abebetiku <47992643+abebetiku@users.noreply.github.com>
2019-03-04 10:24:02 -0500

    stateless functional components  added

---
+++ src/features/user/Settings/BasicPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const BasicPage= () =>{
+return (<div>
+<h1> Basic Page</h1>
+</div>)
+}
+export default BasicPage
---
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const SettingsDashboard = () =>{
+return (<div>
+<h1> Settings Dashboard Page</h1>
+</div>)
+}
+export default SettingsDashboard
